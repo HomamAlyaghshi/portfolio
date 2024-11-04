9ef1f1f2503af49853a33472332ceb5d53db499b
Homam Alyaghshi <homamalyaghshi@gmail.com>
2024-11-04 21:45:56 +0300

    project page added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,26 +3,34 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="w-full h-full">
-      <div className="px-16 py-10  my-10 mx-24 grid grid-cols-2 justify-center items-center gap-10">
+      <div className="px-16 py-10  my-10 mx-24 grid grid-cols-2 justify-center items-center gap-10 font-mono">
         <div className="grid  justify-center items-center gap-4">
+            <h1 className="text-center text-2xl">First Project</h1>
           <img alt="project1" src="/images/project1.png" />
           <button className="btn btn-primary">Live</button>
           <button className="btn btn-accent">&lt;Source/&gt;</button>
 
         </div>
         <div className="grid justify-center items-center gap-4">
+        <h1 className="text-center text-2xl">Furnishify</h1>
+
           <img alt="project2" src="/images/project2.png" />
           <button className="btn btn-primary">Live</button>
           <button className="btn btn-accent">&lt;Source/&gt;</button>
+          
 
         </div>
         <div className="grid justify-center items-center gap-4">
+        <h1 className="text-center text-2xl">Foodisphere</h1>
+
           <img alt="project3" src="/images/project3.png" />
           <button className="btn btn-primary">Live</button>
           <button className="btn btn-accent">&lt;Source/&gt;</button>
 
         </div>
         <div className="grid justify-center items-center gap-4">
+        <h1 className="text-center text-2xl">Portfolio</h1>
+
           <img alt="project4" src="/images/project4.png" />
           <button className="btn btn-primary">Live</button>
           <button className="btn btn-accent">&lt;Source/&gt;</button>
