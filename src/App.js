fc53dd62a41b9ed15b1e292f0d2cbf4be9594873
Homam Alyaghshi <homamalyaghshi@gmail.com>
2024-11-06 17:22:10 +0300

    Dark and Light mode

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import React, { Suspense, lazy } from "react";
 import "./index.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import AboutPage from "./pages/AboutPage";
-import ProjectsPage from "./pages/ProjectsPage";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const ProjectsPage = lazy(() => import("./pages/ProjectsPage"));
 
 function App() {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/aboutpage" exact component={AboutPage} />
-          <Route path="/projectspage" exact component={ProjectsPage} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/aboutpage" exact component={AboutPage} />
+            <Route path="/projectspage" exact component={ProjectsPage} />
+          </Switch>
+        </Suspense>
       </div>
     </Router>
   );
